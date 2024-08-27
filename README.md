c2d7c018f53d25844d795067011263a0f8a0206a
Kenneth Marcano <kennethmarcano96@outlook.com>
2024-08-27 15:11:40 -0300

    docs: readme atualizado

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-### Requisitos
+# Documentacao
+
+## Requisitos
 
 Precisa instalar:
 
@@ -20,14 +22,48 @@ git clone https://github.com/KennethMarcano/Blog-Django.git
 pip install -r requirements.txt
 ```
 
-3) Executa o servidor:
+3) Faz as migrações: 
+```
+python manage.py migrate  
+```
+4) Executa o servidor:
 
 ```
 python3 manage.py runserver
 ```
 
-4) Para os testes:
+5) Para os testes:
 
 ```
 python3 manage.py test
 ```
+
+# Desenvolvimento do projeto
+
+Para desenvolver o projeto primeiro foi necessario familiarizarme com Django, porque só tinha programado em python mas nunca usei Django, comecei lendo a documentação oficial e forums em internet para ter uma introdução clara aos fundamentos da framework, como a configurção do projeto, crear models, views e templates.
+
+Comecei com o basico, conferindo o que precisava instalar, neste caso tive que instalar django o python3 já estava instalado no notebook.
+
+Primeiro criei o projeto com:
+```
+django-admin startproject
+```
+
+Na sequencia criei a aplicação 'blog' com:
+```
+python manage.py startapp
+```
+
+Depois seguendo os forum e a documentaçao criei os models para los Posts e comentarios e fiz as migrações.
+
+Apos isso fiz as views para logica da aplicação, nesta parte tive que ler muito, ver videos e ate usar ChatGPT, nessa parte mexe durante todo o desenvolvimento da app.
+
+Fiz tambem os formularios para criar ou editar os post e comentarios.
+
+Configurei as rotas da app no arquivo urls.py.
+
+Criei os templates para mostrar a lista de posts, un post em especifico com seus comentarios e para editar os posts e comentarios,  estilizei os template com css y usei um pouco de js para mudar alungs atributos.
+
+Parece na sequencia mas na verdade trabalhei en tudo quase que ao mesmo tempo, um dependia do outro entao primeiro fiz para mostrar a rota principal onde ficam os posts e partindo disso fiz o resto como editar um post, ver um post em especifico com seus comentarios.
+
+Depois de conferir que as funcionalidades estavam prontas, fiz os testes, onde tive que pesquisar tambem mas despois de entender 2 exemplos consegui desenvolver os testes.
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404  
+from django.views.decorators.http import require_POST  
 from .models import Post, Comment  
 from .forms import PostForm, CommentForm  
 
 def post_list(request):  
-    posts = Post.objects.all()
-    post_form = PostForm()
-    
-    if request.method == 'POST':  
-        if 'post_form' in request.POST:  
-            post_form = PostForm(request.POST)  
-            if post_form.is_valid():  
-                post_form.save()  
-                return redirect('post_list')  
-    else:  
-        post_form = PostForm()  
+    posts = Post.objects.all()  
+    post_form = PostForm(request.POST or None)  
+
+    if post_form.is_valid():  
+        post_form.save()  
+        return redirect('post_list')  
 
     return render(request, 'blog/post_list.html', {  
         'posts': posts,  
@@ -24,7 +20,7 @@ def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)  
     comment_form = CommentForm(request.POST or None)  
 
-    if request.method == 'POST' and comment_form.is_valid():  
+    if comment_form.is_valid():  
         comment = comment_form.save(commit=False)  
         comment.post = post  
         comment.save()  
@@ -37,50 +33,38 @@ def post_detail(request, post_id):
 
 def post_edit(request, post_id):  
     post = get_object_or_404(Post, id=post_id)  
-    if request.method == 'POST':  
-        post_form = PostForm(request.POST, instance=post)  
-        if post_form.is_valid():  
-            post_form.save()  
-            return redirect('post_list')  
-    else:  
-        post_form = PostForm(instance=post)  
+    post_form = PostForm(request.POST or None, instance=post)  
+
+    if post_form.is_valid():  
+        post_form.save()  
+        return redirect('post_list')  
 
     return render(request, 'blog/post_edit.html', {  
         'post_form': post_form,  
         'post': post,  
     })  
 
+@require_POST  
 def post_delete(request, post_id):  
     post = get_object_or_404(Post, id=post_id)  
-    if request.method == 'POST':  
-        post.delete()  
-        return redirect('post_list')  
+    post.delete()  
+    return redirect('post_list')  
 
-    return render(request, 'blog/post_delete.html', {  
-        'post': post,  
-    })
+def comment_edit(request, post_id, comment_id):  
+    comment = get_object_or_404(Comment, id=comment_id)  
+    comment_form = CommentForm(request.POST or None, instance=comment)  
 
-def comment_edit(request, post_id, comment_id):
-    comment = get_object_or_404(Comment, id=comment_id)
-    if request.method == 'POST':  
-        comment_form = CommentForm(request.POST, instance=comment)  
-        if comment_form.is_valid():  
-            comment_form.save()  
-            return redirect('post_detail', post_id=post_id)  
-    else:  
-        comment_form = CommentForm(instance=comment)  
+    if comment_form.is_valid():  
+        comment_form.save()  
+        return redirect('post_detail', post_id=post_id)  
 
     return render(request, 'blog/comment_edit.html', {  
         'comment_form': comment_form,  
         'comment': comment,  
-    })
+    })  
 
+@require_POST  
 def comment_delete(request, post_id, comment_id):  
     comment = get_object_or_404(Comment, id=comment_id)  
-    if request.method == 'POST':  
-        comment.delete()  
-        return redirect('post_detail', post_id=post_id)  
-
-    return render(request, 'blog/comment_delete.html', {  
-        'comment': comment,  
-    })
+    comment.delete()  
+    return redirect('post_detail', post_id=post_id)  
